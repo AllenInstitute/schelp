e7ed2784e4d0745f3e080df047eb85788fe8e512
Rohan <rhngla@gmail.com>
2024-07-01 12:36:55 -0700

    first commit

---
+++ schelp/utils/config.py
@@ -0,0 +1,33 @@
+def load_config(dataset_key="test", verbose=False):
+    """Loads dictionary with path names and any other variables set through config.toml
+
+    Args:
+        verbose (bool, optional): print paths
+
+    Returns:
+        config: dict
+    """
+    import toml
+    from pathlib import Path
+
+    package_dir = Path(__file__).parent.parent.parent.absolute()
+    config_file = package_dir / "config.toml"
+    
+    if verbose:
+        print(f"package dir: {package_dir}")
+        print(f"config file: {config_file}")
+
+    config = dict()
+    toml_dict = toml.load(config_file)
+    config.update(toml_dict[dataset_key])
+    config.update({"package_dir": package_dir, "config_file": config_file})
+    not_found = []
+    for key in config:
+        if Path(config[key]).exists():
+            config[key] = Path(config[key])
+
+    for key in not_found:
+        print(f"Did not find {key}")
+        config.pop(key, None)
+
+    return config
---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="schelp",
+    description="Source separation for multiplexed modulator imaging",
+    packages=["schelp"],
+    python_requires='>=3.7',
+)
