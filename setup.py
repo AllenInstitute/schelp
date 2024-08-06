33821dfec3b67ce476f8d1c1246d358b84cecef4
Rohan <rhngla@gmail.com>
2024-08-06 11:53:48 -0700

    switching to pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="schelp",
-    description="notebooks and helper functions for tests with transformer models",
-    packages=["schelp"],
-    python_requires='>=3.7',
-)
